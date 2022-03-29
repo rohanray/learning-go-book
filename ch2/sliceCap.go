da02b0a5030bae6bcec3833423457a6e288332b2
Rohan Ray <rohanray@gmail.com>
2022-03-29 11:14:16 +0530

    init ch2

---
+++ ch2/sliceCap.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	println("hello ch2")
+	capacity()
+	slicingSlices()
+}
+
+func capacity() {
+	var x []int
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 10)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 20)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 30)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 40)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 50)
+	fmt.Println(x, len(x), cap(x))
+}
+
+func slicingSlices() {
+	x := []int{1, 2, 3, 4}
+	y := x[:2]
+	z := x[1:]
+	d := x[1:3]
+	e := x[:]
+	fmt.Println("x:", x)
+	fmt.Println("y:", y)
+	fmt.Println("z:", z)
+	fmt.Println("d:", d)
+	fmt.Println("e:", e)
+}
